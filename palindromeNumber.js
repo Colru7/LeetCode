3e4023f632f74636342c58508a09bc6ca3d02ad4
Gipson Oliveros <gipson.oliveros96gmail.com>
2023-03-22 16:35:03 -0400

    First commit of palindrome number

---
+++ palindromeNumber.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+let isPalindrome = function(x) {
+    if(x < 0) return false;
+    if (x % 10 === 0 && x !== 0) return false;
+
+    let reversedNumber = 0;
+
+    while (x > reversedNumber){
+        reversedNumber = reversedNumber * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+
+    return x === reversedNumber || x === Math.floor(reversedNumber / 10);
+};
